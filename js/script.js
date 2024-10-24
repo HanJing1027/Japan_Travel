470e3d308129a92fecda9d7e9f48d27927f71c79
hohohohola_1027 <leran417@gmail.com>
2024-10-24 16:13:57 +0800

    解決管理與瀏覽器動畫加載問題

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// load
 document.addEventListener("DOMContentLoaded", function () {
   fetch("header.html")
     .then((response) => response.text())
@@ -10,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function () {
     .catch((error) => console.error("Error loading header:", error));
 });
 
+// show menu
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId);
   const nav = document.getElementById(navId);
@@ -19,3 +21,12 @@ const showMenu = (toggleId, navId) => {
     toggle.classList.toggle("show-icon");
   });
 };
+
+// animation
+window.addEventListener("load", () => {
+  const elements = document.querySelectorAll(".section__text, .section__btn");
+
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].style.animationPlayState = "running";
+  }
+});
